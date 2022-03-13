8d5c0c80006d516672fffb9d346126e54f039f2b
dragonburrrn <nastyapetrova197@yande.ru>
2022-03-13 23:17:30 +0300

    homework 4 + wikipedia

--- Telebot3.py
+++ Telebot3.py
@@ -4,7 +4,11 @@ import telebot  # pyTelegramBotAPI	4.3.1
 from telebot import types
 import requests
 import bs4
-from random import randint
+
+import wikipedia, re
+
+#русский язык в википедии
+wikipedia.set_lang("ru")
 
 bot = telebot.TeleBot('5204365820:AAFvuyokcKcxoDygSTsQ4I60SkiHMPNK8QU')  # Создаем экземпляр бота @Ivanov_Ivan_1MD19_bot
 
@@ -32,7 +36,7 @@ def get_text_messages(message):
     if ms_text == "Главное меню" or ms_text == "Вернуться в главное меню":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Развлечения")
-        btn2 = types.KeyboardButton("WEB-камера")
+        btn2 = types.KeyboardButton("Wikipedia")
         btn3 = types.KeyboardButton("Управление")
         back = types.KeyboardButton("Помощь")
         markup.add(btn1, btn2, btn3,back)
@@ -54,8 +58,11 @@ def get_text_messages(message):
 
     elif ms_text == "Прислать анекдот":
         bot.send_message(chat_id, text=get_anekdot())
-    elif ms_text == "WEB-камера":
-        bot.send_message(chat_id, text="еще не готово...")
+    elif ms_text == "Wikipedia":
+        bot.send_message(chat_id, 'Отправьте любое слово, и я найду его значение на wikipedia')
+
+
+
     elif ms_text == "Управление":
         bot.send_message(chat_id, text="еще не готово...")
     elif ms_text == "Помощь" or ms_text == "/help":
@@ -66,7 +73,7 @@ def get_text_messages(message):
         img = open('Петрова Анастасия.jpg', 'rb')
         bot.send_photo(message.chat.id, img, reply_markup=key1)
     else:
-        bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
+        bot.send_message(message.chat.id, getwiki(message.text))
 
 # ----------------------------------------------------------------------------
 def get_anekdot():
@@ -78,6 +85,31 @@ def get_anekdot():
         array_anekdots.append(result.getText().strip())
     return array_anekdots[0]
 # -----------------------------------------------------------------------
+
+# чистим текст статьи в wikipedia и ограничиваем его тысячей символов
+def getwiki(s):
+
+    print('fff')
+
+    try:
+        ny = wikipedia.page(s)
+        wikitext = ny.content[:1000]
+        wikimas = wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not ('==' in x):
+                if (len((x.strip())) > 3):
+                    wikitext2 = wikitext2 + x + '.'
+            else:
+                break
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('{[^\{\}]*\)', '', wikitext2)
+        return wikitext2
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+
 
 bot.polling(none_stop=True, interval=0) # Запускаем бота
 
